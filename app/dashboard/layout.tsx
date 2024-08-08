b164ff4fc87041a067ae1216b65ce4ade1808a1d
OmarMSDZ <Omarjadismoralesdiaz@gmail.com>
2024-08-07 23:04:37 -0400

    primer commit

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+//los layouts sirven para crear interfaces que se usan en multiples paginas
+
+//este sidenav es un componente 
+import SideNav from '@/app/ui/dashboard/sidenav';
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+//de esta forma se crea una vista en next, las rutas se definen por el sistema de archivos
+
+export default function Page() {
+    return <p>Dashboard Page</p>;
+  }
